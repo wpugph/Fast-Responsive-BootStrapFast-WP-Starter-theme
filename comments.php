35dd360041637975dcbdda76fdfce922e9f085d6
carl alberto <cralberto11@gmail.com>
2017-02-11 04:58:11 +0800

    updated some doc block

--- comments.php
+++ comments.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
+ * The template for displaying comments.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
--- header.php
+++ header.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
+ * The header.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
