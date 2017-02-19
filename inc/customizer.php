864bd4182af857d0ee5b2a197f24b0d3bec7dcb4
carl alberto <cralberto11@gmail.com>
2017-02-19 16:39:55 +0800

    brunch now integrated

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,7 +21,7 @@ add_action( 'customize_register', 'bootstrapfast_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function bootstrapfast_customize_preview_js() {
-	wp_enqueue_script( 'bootstrapfast_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20170101', true );
+	wp_enqueue_script( 'bootstrapfast_customizer', get_template_directory_uri() . '/assets/admin/js/customizer.js', array( 'customize-preview' ), '20170101', true );
 }
 add_action( 'customize_preview_init', 'bootstrapfast_customize_preview_js' );
 
